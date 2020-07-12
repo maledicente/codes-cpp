187528e83839c4b9098a6a78a694712c6b4ae3ab
maledicente <luizpaulonievola@ymail.com>
2020-07-12 19:04:55 -0300

    Add

--- codes/somaIntervalo.cpp
+++ codes/somaIntervalo.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <conio.h>
-#include <cstdlib>
 using namespace std;
 
-int main(){
+int main(void){
 
-    int sum=0, inicio, fim, intervalo, i, j;
+    int sum, *arr=nullptr, inicio, fim, intervalo, i, j;
 
     cout << "Insina o primeiro numero do intervalo: ";
     cin >> inicio;
     cout << "Insina o ultimo numero do intervalo: ";
     cin >> fim;
     intervalo=fim-inicio;
-    int arr[intervalo];
+    arr = new int [intervalo];
 
     for(j=0; j <= intervalo; j++){
             arr[j]=inicio+j;
         }
-
+    sum=0;
     for(i = 0; i <= intervalo; i++){
         sum+=arr[i];
+        cout << sum << endl;
     }
+    delete [] arr; // delete array pointed
 
     cout << "A soma de todos os numeros no intervalor e: " << sum << endl;
 
